0b552b189b602f395747d7a8b8490c27b733c03b
Akshit Sahani <akshit_sahani@hotmail.com>
2017-01-16 14:26:46 -0500

    exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,14 @@
+#Answer 1. 13% tip so multiply 0.13 into price of meal
+tip = 0.13 * 55
+puts tip
+
+#Answer 2. Convert integer into string using .s method on integer
+result="Hello! " + 5.to_s
+puts result
+
+#Answer 3.
+output = 45628 * 7839
+puts "And the output is #{output}"
+
+#Answer 4. Answer is True (because false or true=true)
+puts (10 < 20 && 30 < 20) || !(10 == 11)
