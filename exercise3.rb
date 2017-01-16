5c112cf162a36f6dca5f439809b42e23f7cd35bc
Akshit Sahani <akshit_sahani@hotmail.com>
2017-01-16 14:45:42 -0500

    third commit

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,8 @@
 puts "What is your name?"
 name = gets.chomp
 puts "Hi, #{name}"
+
+current_year=2016
+puts "How old are you?"
+age = (gets.chomp).to_i
+puts "So, I see that you were born in #{(current_year - age).to_s}"
