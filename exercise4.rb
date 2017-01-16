24a6baaac7b6e7b6ade70a0f7db15d1bc9c5684b
Akshit Sahani <akshit_sahani@hotmail.com>
2017-01-16 15:40:55 -0500

    final commit exercise4

--- exercise4.rb
+++ exercise4.rb
@@ -1,13 +1,25 @@
 fizzbuzz = (1..100)
 
-for e in fizzbuzz
-  if e % 3 == 0
-    puts "Bit"
-  elsif e % 5 == 0
-    puts "Maker"
-  elsif e % 15 == 0
+#for e in fizzbuzz
+#  if e % 3 == 0
+#    puts "Bit"
+#  elsif e % 5 == 0
+#    puts "Maker"
+#  elsif e % 15 == 0
+#    puts "BitMaker"
+#  else
+#    puts e
+#  end
+#end
+
+fizzbuzz.each do |num|
+  if num % 3 == 0 and num % 5 == 0
     puts "BitMaker"
+  elsif num % 5 == 0
+    puts "Maker"
+  elsif num % 3 == 0
+    puts "Bit"
   else
-    puts e
+    puts num
   end
 end
